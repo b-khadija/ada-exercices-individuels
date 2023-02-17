94678301a0af11c475d9133237b964f273c4f5ad
b-khadija <khadija.bayssir@gmail.com>
2023-02-17 10:14:44 +0100

    Add parameters to function didIWin

--- exercice2/script.js
+++ exercice2/script.js
@@ -1,29 +1,64 @@
-function didIWin() {
-  let givenNumber = prompt('Choisissez un nombre');
+/*
+function didIWin(givenNumber) {
+  givenNumber = prompt('Choisissez un nombre');
   givenNumber = parseInt(givenNumber);
   let number = 22;
   while (isNaN(givenNumber) == true || givenNumber === null) {
     givenNumber = prompt('Donne moi un vrai nombre');
   }
-  if(givenNumber > number) {
-    alert('Plus petit');
-  } else if(givenNumber < number) {
+  if(givenNumber < number) {
     alert('Plus grand');
-  } else {
-    alert('Bravo ! Vous avez deviné le nombre');
+  } if(givenNumber > number) {
+    alert('Plus petit');
+  } else if(givenNumber === number) {
+    return true;
   }
 }
 didIWin();
 
+function gamePlay(){
+  while (didIWin(givenNumber) !== true){
+    givenNumber === number;
+  }
+    alert("Bravo !");
+}
+gamePlay();
+*/
+/* -----------------------Correction--------------------------- */ 
+
+function askNumber() {
+  let givenNumber = prompt('Devinez le nombre entre 0 et 50');
+  while (numberToGuess < 0 || numberToGuess >50) {
+    numberToGuess = parseInt(prompt('Le nombre doit être entre 0 et 50'))
+  }
+  return givenNumber;
+}
+
+askNumber();
+
+function didIWin(numberToGuess ,givenNumber) {
+
+  if (givenNumber < numberToGuess) {
+    alert('Plus grand')
+  } else if(givenNumber > numberToGuess){
+    alert('Plus petit');
+  } else {
+    alert('Bravo vous avez trouvé le nombre')
+    return true;
+  }
+  return false;
+}
 
 function gamePlay() {
-  function didIWin(givenNumber) {
-    if (didIWin(isNaN(givenNumber)) == true) {
-      alert('On arrete');
-    } else {
-      return didIWin(givenNumber);
-    }
+  const numberToGuess = askNumber();
+  let result = false;
+  let tentation = 0;
+
+  while (!result) {
+    tentation++;
+    givenNumber = askNumber();
+    result = didIWin(numberToGuess ,givenNumber);
   }
-  return didIWin();
+  alert(`Partie terminer avec ${tentation} essais`);
 }
 gamePlay();
